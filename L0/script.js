2dd06f69a84d6b9792f9aa70813692c80ce0b5e4
dasha <dashagrgrv121@gmail.com>
2023-10-07 22:20:51 +0400

    add: validation and hover effects

--- L0/script.js
+++ L0/script.js
@@ -3,7 +3,9 @@ const counterContainer = document.querySelectorAll('.list-item__counter'),
     modalDelivery = document.querySelector('.modal-delivery'),
     overlay = document.querySelector('.overlay'),
     payButtons = document.querySelectorAll('.js-btn'),
-    deliveryButtons = document.querySelectorAll('.js-btn-delivery');
+    deliveryButtons = document.querySelectorAll('.js-btn-delivery'),
+    
+    sendFormButton = document.getElementById('send');
       
 function openModal(modal){
     modal.classList.add('display-block');
@@ -17,6 +19,49 @@ function closeModal(modal){
     document.body.style.overflow =  "auto";
 }
 
+function checkValidForm(){
+    const form = document.getElementById('form'),
+          name = document.getElementById('name'),
+          surname = document.getElementById('surname'),
+          email = document.getElementById('email'),
+          phone = document.getElementById('phone'),
+          number = document.getElementById('number'),
+          textPattern = /^[a-zA-Z]+$/,
+          phonePattern = /^(\+\d{1,3}[- ]?)?\d{3}[- ]?\d{3}[- ]?\d{4}$/,
+          numberPattern = /^\d{12}$/,
+          emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; 
+
+          if (textPattern.test(name.value)) {
+            name.classList.remove("invalid");
+          } else {
+            name.classList.add("invalid");
+          }
+
+          if (!!textPattern.test(surname.value)) {
+            surname.classList.remove("invalid");
+          } else {
+            surname.classList.add("invalid");
+          }
+
+          if (phonePattern.test(phone.value)) {
+            phone.classList.remove("invalid");
+          } else {
+            phone.classList.add("invalid");
+          }
+
+          if (emailPattern.test(email.value)) {
+            email.classList.remove("invalid");
+          } else {
+            email.classList.add("invalid");
+          }
+
+          if (numberPattern.test(number.value)) {
+            number.classList.remove("invalid");
+          } else {
+            number.classList.add("invalid");
+          }
+
+}
     
 counterContainer.forEach((item)=>{
 
@@ -66,6 +111,11 @@ deliveryButtons.forEach((item)=>{
         });
     });
 });
+
+sendFormButton.addEventListener('click', function(e){
+    e.preventDefault();
+    checkValidForm();
+})
 
 
 
